123feb2bfb4091df1e010ed12223c14ef77861d7
Nozell <nozellservices101@gmail.com>
2026-02-08 02:52:27 +0000

    Try fixing this error: `Runtime ReferenceError: AlertDialogFooter is not

--- src/app/controller/page.tsx
+++ src/app/controller/page.tsx
@@ -26,6 +26,7 @@ import {
   AlertDialogCancel,
   AlertDialogContent,
   AlertDialogDescription,
+  AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
 } from '@/components/ui/alert-dialog';
