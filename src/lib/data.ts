08be5a1743f6b9cdea2a92ead72216e722e10180
Nozell <nozellservices101@gmail.com>
2026-02-10 23:10:26 +0000

    work on this (_for element <Card>_)

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -8,4 +8,26 @@ export const testimonials: Testimonial[] = [];
 
 export const teamMembers: TeamMember[] = [];
 
-export const socialLinks: SocialLinks = [];
+export const socialLinks: SocialLinks = [
+    {
+        "id": 1,
+        "platform": "facebook",
+        "url": "https://www.facebook.com/savieroyal",
+        "is_active": true,
+        "display_order": 1
+    },
+    {
+        "id": 2,
+        "platform": "instagram",
+        "url": "https://www.instagram.com/savieroyal",
+        "is_active": true,
+        "display_order": 2
+    },
+    {
+        "id": 3,
+        "platform": "twitter",
+        "url": "https://www.twitter.com/savieroyal",
+        "is_active": false,
+        "display_order": 3
+    }
+];
